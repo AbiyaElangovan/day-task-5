64710b05b35d8d72bea6c640c1b2a3b866286f37
AbiyaElangovan <abielangovan1@gmail.com>
2023-08-24 13:47:19 +0530

    script

--- script.js
+++ script.js
@@ -128,9 +128,9 @@ console.log(prime(num))
         }
     }
     return result;
- }
- console.log(result)
- )([3,4,5,6,7,8,9,10,11,12,13,17,19,23])
+    console.log(result);
+});
+ ([3,4,5,6,7,8,9,10,11,12,13,17,19,23])
 //OUTPUT:
  //3,5,7,11,13,17,19,23
 //E.palindrome or not:
